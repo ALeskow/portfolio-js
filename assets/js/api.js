cfd11bdee0a4407894a1f8fea7a6d88d3ef1fe4e
ALeskow <anarleskow07901@gmail.com>
2025-08-28 21:31:11 -0300

    more files and modified json content

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,7 @@
 
 async function fetchProfileData() {
-    const url = '/data/profile.json'; // Fetch from local server
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url);
     const profileData = await response.json();
     return profileData;
 }
-
-'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
